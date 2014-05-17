58ba0b470cc4b29aaf1fe1f48cca71c7a0f327bf
sjaved <sjaved@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-17 20:06:23 +0000

    Major Update.
    git-svn-id: https://plugins.svn.wordpress.org/easy-facebook-likebox/tags/1.0.2@916234 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/views/admin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Represents the view for the administration dashboard.
+ *
+ * This includes the header, options, and other information that should provide
+ * The User Interface to the end user.
+ *
+ * @package   Plugin_Name
+ * @author    Your Name <email@example.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2014 Your Name or Company Name
+ */
+?>
+
+<div class="wrap">
+
+	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+    
+    <p>Thanks for installing the plugin.</p>
+    
+    <p>Here are steps and ways you can use this plugin.
+    		<ol>
+            	<li> Add the "Easy Facebook Likebox" widget in sidebar to show the like box in your website sidebar</li>
+                <li> Add widget in sidebar and generate shortcode by saving the widgets optiosn to show likebox somewhere else on your website.</li>
+                <li> You can also directly add below shortcode (without quotes) in post/page editor and change default values with your custom one according to your needs and requirments.
+                "[efb_likebox fanpage_url="https://www.facebook.com/jwebsol" fb_appid="" box_width="300" box_height="" colorscheme="light" show_faces="1" show_header="1" show_stream="0" show_border="1" ]"
+                </li>
+             </ol>
+            </p>
+     <p>I am working on the betterment and improvement of this plugin so stay tuned.</p>       
+
+	<!-- @TODO: Provide markup for your options page here. -->
+
+</div>
