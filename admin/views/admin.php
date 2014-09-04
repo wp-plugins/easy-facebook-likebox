7b5d56b8ee53cc7a758de43c6d8afe59b1e61047
sjaved <sjaved@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-04 12:56:35 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/easy-facebook-likebox/trunk@979244 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -28,19 +28,7 @@
                 </li>
              </ol>
             </p>
-     <p>I am working on the betterment and improvement of this plugin so stay tuned.</p> 
-     
-     Support us by liking us on facebook!
-     
-     <div class="fb-like-box" data-href="https://www.facebook.com/jwebsol" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="true"></div>
-     <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=517129121754984&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>       
+     <p>I am working on the betterment and improvement of this plugin so stay tuned.</p>       
 
 	<!-- @TODO: Provide markup for your options page here. -->
 
