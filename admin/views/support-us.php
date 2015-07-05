deb4fef52d6b421e5f96fb796bed843215e24e58
sjaved <sjaved@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-05 15:28:43 +0000

    New tags/4.0
    git-svn-id: https://plugins.svn.wordpress.org/easy-facebook-likebox/tags/4.0@1192816 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/views/support-us.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Represents the view for the administration of support us section.
+ *
+ * This includes the header, options, and other information that should provide
+ * The User Interface to the end user.
+ *
+ * @package   Plugin_Name
+ * @author    Your Name <email@example.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2014 Your Name or Company Name
+ */
+				
+?> 
+<div class="fb-like-box" data-href="https://www.facebook.com/jwebsol" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="true"></div>
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=517129121754984&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
